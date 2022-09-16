bb7fa0bbe47ed1e8aee0ddac72867ea483e245f4
evanrich2404 <evanrich2404@gmail.com>
2022-09-16 08:18:30 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints Programming is like a building a multilingual puzzle
+ * Return: always 0
+ */
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,\n);
+}
