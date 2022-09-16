211d51d72ae9a082c75db28c0de446a49887ff55
evanrich2404 <evanrich2404@gmail.com>
2022-09-16 07:40:09 -0700

    myfirstC

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@ int main(void)
 {
 	/* making my string to print */
 	char Mystr[] = "Programming is like building a multilingual puzzle"
-	/* using puts to print the str */
+;	/* using puts to print the str */
 	puts(Mystr);
 	return (0);
 }
