348f37250b01ec00b27b722c429f9d9bca507a72
evanrich2404 <evanrich2404@gmail.com>
2022-09-19 13:01:23 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* print base 10 by Evan R. */
+/**
+ * main - printing base 10
+ * Return: is 0
+ */
+int main(void)
+{
+	int n = '0';
+	/* printing base 10 */
+		while (n <= 9)
+			putchar(n);
+			n++;
+	return (0);
+}
