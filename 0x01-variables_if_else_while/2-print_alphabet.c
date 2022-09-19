997786b6ecbe08f4d0b939f40f4150cf98209932
evanrich2404 <evanrich2404@gmail.com>
2022-09-19 11:15:29 -0700

    printalpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* printing the alphabet by Evan R. */
+
+/**
+ * main - printing alphabet
+ * Return: is 0
+ */
+int main(void)
+{
+	int ch;
+	/* print all lowercase letters */
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+}
