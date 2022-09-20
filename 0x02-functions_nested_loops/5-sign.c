915736f1bc90d4838b0ad3c9afb9860b5b5a4c9e
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 11:37:10 -0700

    printsign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a sign of the number (+, 0, -)
+ * @n: single digit number or integer
+ * Return: 1 if greater than zero, 0 if is zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		print_sign(+);
+	else if (n = 0)
+		return (0);
+		print_sign(0);
+	else
+		return (-1);
+		print_sign(-);
+}
