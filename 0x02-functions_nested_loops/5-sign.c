872d9994cdd05b89e29af8bf191a24223e239a61
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 11:41:18 -0700

    printsign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
 int print_sign(int n)
 {
 if (n > 0)
+	print_sign('+');
 	return (1);
-		print_sign('+');
 else if (n == 0)
+	print_sign('0');
 	return (0);
-		print_sign('0');
 else
+	print_sign('-');
 	return (-1);
-		print_sign('-');
 }
