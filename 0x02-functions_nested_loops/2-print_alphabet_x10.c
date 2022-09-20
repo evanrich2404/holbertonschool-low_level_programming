f4c28f56dbc5a9f8f41a3526ac8c4ec8bcaceb54
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 09:22:20 -0700

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,8 +6,14 @@
 void print_alphabet_x10(void)
 {
 	char O, L;
+	{
+	for (O = 0; O < 9; O++)
+	}
 
-	for (O = (L = 'a'; L <= 'z'; L++);)
-	_putchar(O);
+	for (L = 'a'; L <= 'z'; L++)
+	{
+		_putchar(L);
+	}
 	_putchar('\n');
+
 }
