02951a4a7703ce5b4907d98a7c2fbc2b0b81fdb3
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 08:51:58 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing it 10 times
+ */
+void print_alphabet_x10(void)
+{
+	for (L = 'a'; L <= 'z'; L++)
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar(L);
+		_putchar('\n');
+}
