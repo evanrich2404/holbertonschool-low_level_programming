a08231c4a46d05f639266bfd2b4b8205f4061620
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 09:57:28 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,9 @@
  */
 int _islower(int c)
 {
-	int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
+	c = _islower(%c);
+	_putchar(c + '0')
 	_putchar('\n');
 	return (0);
 }
