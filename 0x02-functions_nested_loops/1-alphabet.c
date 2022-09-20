8e9725487064aa2aa53154b99a2f97fe8e5cb41f
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 08:32:48 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* printing alphabet by Evan R */
+/**
+ * print_alpabet - using print_alphabet to print
+ */
+void print_alphabet(void)
+{
+	char L;
+
+	for (L = 'a'; L <= 'z'; L++)
+		_putchar('L')
+
+		_putchar('\n')
+}
