62b9e4225b0455720192ebc19e92b7cda979e763
evanrich2404 <evanrich2404@gmail.com>
2022-09-20 08:16:12 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
-
 /**
  * File: main.h
  * Evan Richardson
  * This is a header file to use prototype commands
  */
 
-int _putchar;
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
