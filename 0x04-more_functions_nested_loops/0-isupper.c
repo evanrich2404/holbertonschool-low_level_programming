d4a7bd799dba75afc6bcc0e4749fcfd73b5b2d65
evanrich2404 <evanrich2404@gmail.com>
2022-09-22 08:35:37 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,13 @@
 
 /**
  * _isupper - function to show if letters are uppercase
- * @c: letter input
+ * @c: single letter input
  * Return: 1 if c is uppercase, 0 if otherwise
  */
 int _isupper(int c)
 {
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
