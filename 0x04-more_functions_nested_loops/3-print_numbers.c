364cc926209d6845b0f82c334e3ad57067bfaec0
evanrich2404 <evanrich2404@gmail.com>
2022-09-22 11:45:33 -0700

    printnumbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
 
 void print_numbers(void)
 {
-	int n = '0';
+	int n;
 
-	for (n >= '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(('n' + '0'));
+		_putchar(n + '0');
 	}
 		_putchar('\n');
 }
