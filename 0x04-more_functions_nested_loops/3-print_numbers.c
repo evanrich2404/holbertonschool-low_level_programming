525ca2aaca7e78841890b520e87715316923d702
evanrich2404 <evanrich2404@gmail.com>
2022-09-22 09:52:12 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers followed by a new line
+ * Return: is 0
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	for (n >= 0; n <= 9; n++)
+		_putchar(('n' + 0));
+		_putchar('\n');
+		return (0);
+}
