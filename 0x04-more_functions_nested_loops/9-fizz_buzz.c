b6a8766ec79266532cf4d0f59e4a250d03aa1ce4
evanrich2404 <evanrich2404@gmail.com>
2022-09-23 08:54:05 -0700

    FizzBuzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,18 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%d ", n);
+		{
+			if (n < 100)
+				putchar(' ');
+		}
 	}
 	putchar('\n');
+	return (0);
 }
