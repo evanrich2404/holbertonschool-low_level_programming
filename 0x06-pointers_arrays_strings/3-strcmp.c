8c2e4020fa2463536701a2403d20622b5436ce37
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 16:00:39 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (*s1 == '\0' || *s2 == '\0')
-			break;
 		s1++;
 		s2++;
 	}
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == *s2)
 		return (0);
-	else if (*s1 > 0 && *s2 > 0)
-		return (1);
 	else
-		return (-1);
+		return (*s1 - *s2);
 }
