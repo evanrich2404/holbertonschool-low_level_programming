ef3c7a5f488c505b3985d270604962f0a248e990
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 13:08:59 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,19 @@
 
 /**
  * _strncpy - should work the same as strncpy
- *
+ * @dest: input string
+ * @src: input string
+ * @n: input integer of bytes
+ * Return: dest
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
