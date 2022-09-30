bbae11db08292aa3c7ab69e543a8772061651e87
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 13:22:07 -0700

    save&stop

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - should function like strcmp
+ * @s1: input string
+ * @s2: input string
+ * Return: < 0, if s1<s2; > 0, if s1>s2; = 0, if s1==s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
