867036a51b079093fb9e9ba6b6f9f1ff9a672090
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 11:10:18 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
+#include "_strlen.c"
 
 /**
- * 
- *
+ * _strcat - appends src to dest ow termnull end with new null
+ * Return: pointer to the resulting string dest
+ * @dest: string input
+ * @src: string input
  */
+
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = _strlen(dest);
+	size_t n, i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}
