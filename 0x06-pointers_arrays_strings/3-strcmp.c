efcc86c5facb22916736ab4293246f373596209c
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 15:43:52 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,22 @@
  * _strcmp - should function like strcmp
  * @s1: input string
  * @s2: input string
- * Return: < 0, if s1<s2; > 0, if s1>s2; = 0, if s1==s2
+ * Return: < 0, if s1<s2, > 0, if s1>s2, = 0, if s1==s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0' || *s2 == '\0')
+			break;
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else if (*s1 > 0 && *s2 > 0)
+		return (1);
+	else
+		return (-1);
+}
