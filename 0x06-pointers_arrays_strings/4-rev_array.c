8f8ae0c430fcfb130b8fab352c444672f6829d88
evanrich2404 <evanrich2404@gmail.com>
2022-09-30 18:07:19 -0700

    rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,9 +11,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int t = 0;
 
-	for (i = 0; i < n; i++)
-		_putchar(a[i]);
-	for (i = n - 1; i >= 0; i--)
-		_putchar(a[i]);
+	for (i = 0; i < (n / 2); i++)
+	{
+
+		t = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
+	}
 }
