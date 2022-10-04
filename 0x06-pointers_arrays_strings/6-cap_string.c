17668a32ee36b4e627a47ad095d72f060324c4f4
evanrich2404 <evanrich2404@gmail.com>
2022-10-04 13:09:07 -0700

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,4 +8,22 @@
 
 char *cap_string(char *s)
 {
+	int f;
 	int i = 0;
+	char j[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	
+while (s[i] != '\0')
+	{
+	for (f = 0; s[i] != j[f]; f++)
+		{
+		if ((s[i] + 1) >= 'a' && (s[i] + 1) <= 'z')
+			{
+				_putchar((s[i] + 1) - 32);
+			}
+		else
+			_putchar(s[i] + 1);
+		}
+	i++;
+	}
+	return (s);
+}
