e3b8f7d4a3da1f0a8f1c888c97c5e613af14b2b5
evanrich2404 <evanrich2404@gmail.com>
2022-10-07 11:16:57 -0700

    sqrtrecursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
+
+/**
+ * helper - helps _sqrt_recursion use recursion
+ * @n: integer input
+ * @i: root variable
+ * Return: i
+ */
+int helper(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (helper(n, i + 1));
+	
+}
 /**
  * _sqrt_recursion - returns square root of a number
- * Return: 
+ * Return: -1 on failure, otherwise sqrt n
+ * @n: integer input
  */
 int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper(n, 0));
+}
