93559d528377a49af0202b3843848f7b6d2a097c
evanrich2404 <evanrich2404@gmail.com>
2022-10-10 13:23:53 -0700

    simple code for printing file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * whatsmyname - function that prints its name follwed by \n
+ * Description: must be able to work even if name has been changed
+ * @argv: is an array
+ * Return: is 0
+ */
+char whatsmyname(char *argv[])
+{
+	printf("%s", argv[0]);
+		return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+learning how argc and argv work and how to use __attribute__((unused)) or (void) to compile functions with unused variables or parameters
