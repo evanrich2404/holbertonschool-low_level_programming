2f31d96cdd47f005edd6dd39502cdf700069ad31
evanrich2404 <evanrich2404@gmail.com>
2022-10-11 16:56:27 -0700

    allocgrid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,9 +3,32 @@
  * alloc_grid - returns a ptr to a 2D array of ints
  * @width: input for width
  * @height: input for height
- * Return: 0
+ * Return: allocated grid
  */
 int **alloc_grid(int width, int height)
 {
-	return (0);
+	int **out;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	out = (int **)malloc(sizeof(int *) * height);
+
+	if (out == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		out[i] =malloc(sizeof(int) * width);
+		if (out[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+				free(out[i]);
+			free (out);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			out[i][j] = 0;
+	return (out);
 }
