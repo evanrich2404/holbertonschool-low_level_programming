fdf3f7191412476536ac252c2fc60552855abe82
evanrich2404 <evanrich2404@gmail.com>
2022-10-11 16:32:54 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,9 +2,25 @@
 /**
  * _strdup - returns ptr to new alloc space in mem, contains cp of str as param
  * @str: given string
- * Return: pointer or 0
+ * Return: pointer to a duped string
  */
 char *_strdup(char *str)
 {
-	return (0);
+	char *out;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len++])
+		;
+	out = malloc(sizeof(char) * len);
+	if (out == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		out[i] = str[i];
+	}
+	return(out);
 }
