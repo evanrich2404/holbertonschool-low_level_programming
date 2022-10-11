9762ca320fa866744c65509de723ae8a1c3a6592
evanrich2404 <evanrich2404@gmail.com>
2022-10-11 11:55:29 -0700

    multiply the numbers, good

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,9 +3,22 @@
  * main - multiplies two numbers
  * @argc: counts arguments
  * @argv: array of arguments
- * Return: is 0
+ * Return: is 0, unless it's 1
  */
 int main(int argc, char *argv[])
 {
+	int i, o;
 
+	if (argc > 2)
+	{
+		i = atoi(argv[1]);
+		o = atoi(argv[2]);
+		printf("%d\n", i * o);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
