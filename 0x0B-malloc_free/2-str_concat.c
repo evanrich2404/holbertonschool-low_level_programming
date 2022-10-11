3a9a7d89638e4a41cacd417e58f8e4073f48cf5a
evanrich2404 <evanrich2404@gmail.com>
2022-10-11 16:47:51 -0700

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,9 +3,33 @@
  * str_concat - concatenates two strings
  * @s1: string input one
  * @s2: string input two
- * Return: 0
+ * Return: s1s2\0
  */
 char *str_concat(char *s1, char *s2)
 {
-	return (0);
+	char *out;
+	int d1, d2, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	d1 = d2 = 0;
+
+	while (s1[d1])
+		d1++;
+	while (s2[d2])
+		d2++;
+
+	out = malloc(sizeof(char) * (d1 + d2 + 1));
+
+	if (out == NULL)
+		return (NULL);
+	for (i = 0; i < d1; i++)
+		out[i] = s1[i];
+	for (i = 0; i < d2; i++)
+		out[i + d2] = s2[i];
+	out[d1 + d2 + 1] = '\0';
+
+	return (out);
 }
