3898e3026100466d70e42acf285c3b4fa10a349b
evanrich2404 <evanrich2404@gmail.com>
2022-10-11 17:00:14 -0700

    freegrid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,9 +3,12 @@
  * free_grid - frees a 2D grid previously created by alloc_grid function
  * @grid: input one
  * @height: input two
- * Return: void
  */
 void free_grid(int **grid, int height)
 {
-	return (void);
+	for (; height > 0; height--)
+	{
+		free(grid[height - 1]);
+	}
+	free(grid);
 }
