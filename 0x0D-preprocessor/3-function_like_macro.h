3f17fcfe83d62f0f90cca9e8db0f5c8ae6a56347
evanrich2404 <evanrich2404@gmail.com>
2022-10-15 12:55:15 -0700

    I think this produces absolute value, not sure

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCMACRO_H
+#define FUNCMACRO_H
+/*
+ * File: 3-function_like_macro.h
+ * Trying to create a functionlikemacro that prints absolute value of x
+ */
+#define ABS(x) (x < 0 ? -x : x)
+
+#endif
