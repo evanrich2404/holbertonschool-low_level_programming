5960f3256e7834f77559449d86fa96419a1afd96
evanrich2404 <evanrich2404@gmail.com>
2022-10-17 14:31:01 -0700

    stopping for now

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings
+ * Return: NULL if failure, pointer to new alloc mem containing s1 with n of s2
+ * @s1: input string 1
+ * @s2: input string 2
+ * @n: input variable
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
+}
