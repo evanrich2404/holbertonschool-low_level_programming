f3963df07e2901e6066e72fe7823eed3f9f056ae
evanrich2404 <evanrich2404@gmail.com>
2022-10-18 13:18:49 -0700

    bug checking

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,11 +7,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *mem = malloc(sizeof(nmemb) * size);
+	unsigned int i;
+	char *mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	if (!mem)
+
+	mem = malloc(nmemb * size);
+
+	if (mem == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		mem[i] = 0;
 	return (mem);
 }
