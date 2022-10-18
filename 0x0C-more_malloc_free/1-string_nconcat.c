00c2eac5ee53d860bbcfadeab29abb926e500b91
evanrich2404 <evanrich2404@gmail.com>
2022-10-18 12:20:55 -0700

    problem solving

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i] = s1[i];
 	for (j = 0; j < n && s2[j]; j++)
 		s[i + j] = s2[j];
-	s[i] = '\0';
+	s[i + j] = '\0';
 
 	return (s);
 }
