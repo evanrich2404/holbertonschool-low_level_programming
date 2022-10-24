976f4aee588b2a6bf8ee8f74c17edffece83d2b9
evanrich2404 <evanrich2404@gmail.com>
2022-10-24 13:59:28 -0700

    I feel like I am going to overthink a lot

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: input name pointer
+ * @f: function pointer that takes a char * and doesn't return (void)
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
