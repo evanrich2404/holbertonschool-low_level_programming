2823912480d285c2b914e6b8d1d2cb1a0ecdeee7
evanrich2404 <evanrich2404@gmail.com>
2022-10-23 19:30:57 -0700

    from now on I am going to try using this kind of conditional statement, can't remember what it is called

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+	printf("Name: %s\n", ((*d).name == NULL ? "(nil)" : (*d).name));
 	printf("Age: %f\n", (*d).age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", ((*d).owner == NULL ? "(nil)" : (*d).owner));
 }
