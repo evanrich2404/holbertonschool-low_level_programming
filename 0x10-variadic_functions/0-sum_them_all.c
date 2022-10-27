e3655134077ab0d327e8b51763b55442c148d496
evanrich2404 <evanrich2404@gmail.com>
2022-10-26 18:43:26 -0700

    making function to sum all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include
+/**
+ * sum_them_all - get the sum of all parameters
+ * @n: constant variable
+ * Return: 0 if 0 otherwise return sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	if(n == 0)
+		Return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int)
+	va_end(args);
+
+	return (sum);
+}
