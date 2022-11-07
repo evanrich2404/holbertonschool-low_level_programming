c9e1fb7938f4d3da1bc85af7767204dcb81ba2db
evanrich2404 <evanrich2404@gmail.com>
2022-11-07 14:00:34 -0800

    listint testing faze

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_listint - prints list of ints
+ * @h: input variable
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h)
+	{
+		if (!(h->n))
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d]", h->n);
+		}
+		nodeCount++;
+		h = h->next;
+	}
+	return (nodeCount);
+}
