fec3b65a9608c49159ff6f6948aeab2ea7f9fafb
evanrich2404 <evanrich2404@gmail.com>
2022-11-08 14:38:52 -0800

    typo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		oldlistint_s = oldlistint_s->next;
 	}
 	newNode->next = oldlistint_s->next;
-	oldlistint_t->next = newNode;
+	oldlistint_s->next = newNode;
 	return (newNode);
 }
