2b842c72923c737cf9fdbe8da89f2e4e26619bb5
evanrich2404 <evanrich2404@gmail.com>
2022-11-11 13:25:00 -0800

    no doubly, shortened unnecessary

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -10,14 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h)
 	{
-		if (!(h->n))
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
+		printf("%d\n", h->n);
 		nodeCount++;
 		h = h->next;
 	}
